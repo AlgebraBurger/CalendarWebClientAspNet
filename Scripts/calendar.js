27b4939a78b5f22aa335b76dfce5574e5a0d493d
juliusbacosa <juliusbacosa@gmail.com>
2015-11-06 20:59:31 +0800

    - update with modal

--- Scripts/calendar.js
+++ Scripts/calendar.js
@@ -1,6 +1,7 @@
  $(document).ready(function() {
      $.getScript('Scripts/fullcalendar.min.js', function () {
-			  var date = new Date();
+
+              var date = new Date();
 			  var d = date.getDate();
 			  var m = date.getMonth();
 			  var y = date.getFullYear();
@@ -11,7 +12,10 @@
 			      center: 'title',
 			      right: 'Year,month,agendaWeek,agendaDay'
 			    },
-			    editable: true,
+			    eventRender: function (event, element) {
+			        element.attr('href', 'javascript:void(0);');
+			    },
+			    dayClick: function (date, jsEvent, view) { },
 			    events: function (start, end, callback) {
 			        $.ajax({
 			            url: 'http://localhost:53364/api/events',
@@ -20,27 +24,25 @@
 			                //doc = [{start:"2015-11-10",title:"Mandaloryan"}];
 			                var events = [];
 			                $(doc).each(function () {
-			                    
 			                    var event = [];
-			                    
 			                    var date = new Date($(this).attr('start'));
-			                  
 			                    var newDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
 			                    var t = $(this).attr('title') + " : " + newDate;
 			                    event.title = t.toString();
 			                    event.start = newDate.toString();
-			                    
 			                    events.push(event);
 			                });
-
-			                console.log(events);
-			                
 			                callback(events);
 			              
 			            }
 			        });
 			    },
-		  
+			    eventClick:  function(event, jsEvent, view) {
+			        $('#modalTitle').html(event.title);
+			        $('#modalBody').html(event.description);
+			        $('#eventUrl').attr('href', event.url);
+			        $('#fullCalModal').modal();
+			    },
 		    	dayRender: function (date, cell) {
 			        var today = new Date();
 			        var end = new Date();
